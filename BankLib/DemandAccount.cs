66ea61755cdccf88b332725b1262269c3d1ccf01
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-07-05 13:52:06 +0300

    Revert "add all files"
    This reverts commit a01cf985e9398b7469cf26a0bb009775bbf06913.

--- BankLib/DemandAccount.cs
+++
@@ -1,9 +0,0 @@
-namespace BankLib {
-    public class DemandAccount : Account {
-        public DemandAccount(decimal sum, decimal percentage) : base(sum, percentage) {}
-
-        protected internal override void Open() {
-            OnOpened(new AccountEventArgs($"Открыт новый счет до востребования! Id: {Id}", Sum));           
-        }
-    }
-}
--- BankLib/IAccount.cs
+++
@@ -1,6 +0,0 @@
-namespace BankLib {
-    public interface IAccount {
-        void Put(decimal sum);
-        decimal Withdraw(decimal sum);
-    }
-}
